bed6ff0c95189ee80884cca589c853bdd0338417
muebau <muebau@gmail.com>
2019-02-16 20:44:26 +0100

    Get Mapcrafter version for Minecraft 1.12 working again
    * Fixed dependencies by build Mapcrafter from source
    * split the build and the runtime stages
    * changed logging to output to STDOUT and STDERR to match docker best
    practices

--- render.sh
+++ render.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 if [ ! -f /config/render.conf ]; then
+    echo "No config file found. Created a new one."
     cp /render.conf /config/render.conf
 fi
 
 RUNNINGMAPCRAFTERS=$(pgrep -c mapcrafter)
 if [ $RUNNINGMAPCRAFTERS == "0" ]; then
-    mapcrafter -c /config/render.conf -j 2 >> /var/log/cron.log 2>&1
+    mapcrafter -c /config/render.conf -j 2
 else
-    echo "Found $RUNNINGMAPCRAFTERS instances of mapcrafter. So no new instance was started." >> /var/log/cron.log
+    echo "Found $RUNNINGMAPCRAFTERS instances of mapcrafter. So no new instance was started."
 fi
